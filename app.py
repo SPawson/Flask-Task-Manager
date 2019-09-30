9883190e00eb552726ebbf7875abcb782bdeae78
Sam Pawson <samuel.pawson@gmail.com>
2019-09-30 20:17:40 +0100

    added insert functionality

--- app.py
+++ app.py
@@ -18,8 +18,18 @@ mongo = PyMongo(app) #creates instance of PyMongo
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find()) #second part will store in var tasks, all of the items in the "task" collection
 
+@app.route('/add_task')
+def add_task():
+    _categories= mongo.db.categories.find()
+    category_list= [category for category in _categories]
+
+    return render_template('addtask.html', categories= category_list)
+
+@app.route('/insert_task', methods=["POST"])#When using post , it must be specified as a parameter, default is get
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict()) #uses flask request object to process form-data
+    return redirect(url_for('get_tasks'))
 
 if __name__ == '__main__':
-    
-
     app.run(host = host, port = port, debug=True)
